8df24550c2b69be8325ac03dc26ff8cd9f972820
Markus Moltke <makakwastaken@gmail.com>
2023-09-07 01:32:04 +0200

    Fixed errors with linting

--- src/edi/uneceStructurePageParser.ts
+++ src/edi/uneceStructurePageParser.ts
@@ -179,6 +179,7 @@ export class UNECEStructurePageParser extends UNECEPageParser {
       }
     }
     helper.onOpenTag = (name) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
       if (this.sm.state === State.messageStructureStart && name === 'a') {
         this.sm.transition(State.segmentPosition)
       }
--- src/util.ts
+++ src/util.ts
@@ -35,10 +35,13 @@ function toString(data: any, pretty?: boolean): string {
   const ordered = !Array.isArray(data) // Only order if assoc array
     ? Object.keys(data as { [key: string]: any })
         .sort()
-        .reduce((obj, key) => {
-          obj[key] = data[key] as { [key: string]: any }
-          return obj
-        }, {} as { [key: string]: any })
+        .reduce(
+          (obj, key) => {
+            obj[key] = data[key] as { [key: string]: any }
+            return obj
+          },
+          {} as { [key: string]: any },
+        )
     : data
   if (pretty) {
     return JSON.stringify(ordered, null, 2)
